ced85cd59de2bdd674f67979115d380d9457ce97
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:22:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces => 4}}
+	monopoly = {:railroads => [{:pieces => 4}, {:names => {}}]}
 end
 
 def monopoly_with_third_tier

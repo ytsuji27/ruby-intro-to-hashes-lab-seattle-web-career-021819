b1c20cfa4d527effa2127754eacfafd912ccd157
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:23:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces => 4, :names => {}}}
+	monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
 end
 
 def monopoly_with_third_tier

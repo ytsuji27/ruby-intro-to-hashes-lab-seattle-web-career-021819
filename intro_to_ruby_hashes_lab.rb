c6c5938b871c174463caf7a861c60c8462605532
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:25:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,8 @@ def monopoly_with_third_tier
 	    :pieces => 4, 
 	    :names => {}, 
 	    :rent_in_dollars => {
-	      :one_piece_owned = 25
+	      :one_piece_owned = 25,
+	      :two_pieces_owned = 50
 	    }
 	  }
 	}

a867fa54206520ff0b1599e5b01fce23ed98fa4f
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:29:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,9 @@ def monopoly_with_fourth_tier
 	      :pennsylvania_railroad => {
 	        "mortage_value" => "$200"
 	      },
-	      :b_and_o_railroad => {},
+	      :b_and_o_railroad => {
+	        "mortage_value" => "$400"
+	      },
 	      :shortline => {}
 	    }, 
 	    :rent_in_dollars => {

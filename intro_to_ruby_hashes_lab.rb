f10ac1717e24772ed00bd1b162158ca43c9011ca
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-18 05:27:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
 	    :pieces => 4, 
 	    :names => {
 	      :reading_railroad => {},
-	      :pennsylvania_railroad = {},
+	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
 	    }, 
